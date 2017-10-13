485e40ec0ac8951b4bce1cececeba37f0a6c1747
andi212 <30556321+andi212@users.noreply.github.com>
2017-10-13 21:25:44 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/10/all-about-cancer.html"}
 if(x=="2"){xxx="/2017/10/hepatitis-b-c-d.html"}
 if(x=="3"){xxx="/2017/10/neurodermatitis.html"}
 if(x=="4"){xxx="/2017/10/hiv-and-aids.html"}
-if(x=="5"){xxx="/2017/10/diabetes.html"}a_to_ve[i].href="http://www.otw-link.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/diabetes.html"}a_to_ve[i].href="https://www.otw-link.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
